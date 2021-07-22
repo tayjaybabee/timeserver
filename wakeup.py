cd33366a19097c8fb2a9730d237141129722af80
doubledave <david@boxxy.info>
2021-07-21 23:07:34 -0500

    add

---
+++ wakeup.py
@@ -0,0 +1,77 @@
+from time import sleep, time
+import serial # python3 -m pip install pyserial
+from udp_send import init, send, end, time_32bits
+
+wakeup_string = b"\x3A\x13\x01\x16\x79"
+
+def about_bytestring(wakeup_string):
+    print (type(wakeup_string))
+    print (len(wakeup_string))
+    print (wakeup_string)
+
+def interpret(bytestring):
+    intarray = []
+    for byte in bytestring:
+        intarray.append(byte)
+    #print (len(intarray))
+    #print (intarray)
+    if len(intarray) == 36:
+        #voltage = (intarray[22] * 256 + intarray[21]) / 1000.0
+        #voltage = np.array([intarray[22],intarray[21]], dtype=np.int16)
+        voltage = int.from_bytes([intarray[21],intarray[22]],byteorder="little",signed=False) / 1000.0
+        current = int.from_bytes([intarray[25],intarray[26]],byteorder="little",signed=True)  / 1000.0
+        highcell= int.from_bytes([intarray[29],intarray[30]],byteorder="little",signed=False) / 1000.0
+        lowcell = int.from_bytes([intarray[31],intarray[32]],byteorder="little",signed=False) / 1000.0
+        chargedischarge = ""
+        if current == 0:
+            chargedischarge = ". "
+        else:
+            if current > 0:
+                chargedischarge = " (charging). "
+            else:
+                chargedischarge = " (discharging). "
+
+        difference = round(highcell - lowcell, 4)
+        print (f"{voltage}V {current}A{chargedischarge} Cell with highest voltage at {highcell}V, lowest: {lowcell}V Difference: {difference}V")
+    else:
+        print (f"bytes: {len(bytestring)} Data: {bytestring}")
+
+def main():
+    #about_bytestring(wakeup_string)
+    ser = serial.Serial(
+        port = "/dev/ttyS0",
+        baudrate = 9600,
+        timeout=4.0
+    )
+
+    dest = '192.168.1.255'
+    init() #initialize UDP broadcast
+    #serstring = ser.read_all()
+    while True:
+        last_writetime = time()
+        #print(f"writing {wakeup_string}")
+        ser.write(wakeup_string)
+        #print("reading")
+        serstring = ser.read(size=36)
+        interpret(serstring)
+        if len(serstring) == 36:
+            packet = time_32bits(timestamp=last_writetime) + serstring + b' ' + bytes(str(round(last_writetime, 3)), 'utf-8')
+            send(packet=packet,dest=dest,port=5606)
+
+        secondselapsed = time() - last_writetime
+        if secondselapsed > 0:
+            if secondselapsed < 5.0:
+                sleep(5.0 - secondselapsed)
+                #print(f"   elapsed time before sleep: {secondselapsed}, after sleep: {time() - last_writetime}")
+            else:
+                print (f"   elapsed time: {secondselapsed}")
+        else:
+            print (f"   time counting backwards? seconds elapsed: {secondselapsed}")
+            break
+
+    print("closing serial port")
+    ser.close() #close serial port
+    end()       #close UDP port
+
+if __name__ == "__main__":
+    main()
