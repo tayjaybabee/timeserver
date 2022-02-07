7c4efdc8bb947cf409bb11789b4f775b2cd37994
doubledave <david@boxxy.info>
2022-02-06 18:50:40 -0600

    Display output as color coded including the hex string from the battery pack

--- udp_receive.py
+++ udp_receive.py
@@ -30,16 +30,43 @@ Aborting.')
         okay = False
     while okay:
         data, addr = receive()
-        winsound.Beep(1500, 60)
+        # winsound.Beep(1500, 60) // this gets annoying
         batt_data = data[4:40]
         batt_data_first35 = batt_data[0:35]
         received_crc = batt_data[35]
         calculated_crc = calc(batt_data_first35)
         if received_crc == calculated_crc:
-            print(f"From {addr} Recv Port: \
-{recvport} Bytes: {len(data)}")
+            # print(f"From {addr} Recv Port: {recvport} Bytes: {len(data)}")
             (voltage, current, power, highcell, lowcell, difference, percent,
              temperatures) = interpret(batt_data)
+            avgtemp = (temperatures[0] + temperatures[1] + temperatures[2] + temperatures[3]) / 4.0
+            # batt_data_hex = batt_data.hex()
+            pos = 0
+            for byte in batt_data:
+                byte_hex = hex(byte)[2:].zfill(2)
+                if pos == 5:
+                    print("\033[93m{}\033[00m" .format(byte_hex), end='')  # print % byte in yellow
+                elif (pos > 6 and pos < 11):
+                    print("\033[92m{}\033[00m" .format(byte_hex), end='')  # print temperature bytes in green
+                elif (pos > 24 and pos < 27):
+                    print("\033[91m{}\033[00m" .format(byte_hex), end='')  # print current bytes in red
+                elif (pos > 20 and pos < 23):
+                    print("\033[96m{}\033[00m" .format(byte_hex), end='')  # print voltage bytes in cyan
+                elif (pos > 28 and pos < 33):
+                    print("\033[95m{}\033[00m" .format(byte_hex), end='')  # print cell hi/lo voltage bytes in purple
+                elif (pos == 35):
+                    print("\033[90m{}\033[00m" .format(byte_hex), end='')  # print crc in gray
+                else:
+                    print(byte_hex, end='')                                # print unknown bytes as plain colored text
+                pos += 1
+            print("\033[93m{}\033[00m" .format(" " + str(percent) + "% "), end='')
+            print("\033[92m{}\033[00m" .format(str(avgtemp) + "°C "), end='')
+            print("\033[95m{}\033[00m" .format("lo " + str(lowcell) + "V "), end='')
+            print("\033[96m{}\033[00m" .format(str(voltage) + "V "), end='')
+            print("\033[95m{}\033[00m" .format("hi " + str(highcell) + "V "), end='')
+            print("\033[91m{}\033[00m" .format(str(current) + "A "), end='')
+            print(str(power) + "W", end='')
+            print('')                                                      # new line
         else:
             print(f"CRC doesn't match. expected: {hex(calculated_crc)} got \
 {hex(received_crc)}")
